13ec9d621091f85522afe7e24de6e95b8e49b78c
Igor Nepipenko <igornepipenko@gmail.com>
2019-10-07 21:52:03 +0300

    Feature/phones page (#2) (#3)
    * Feature: create page component [ref:no-ref]
    
    * Feature: add temaplate [ref:no-ref]
    
    * Feature: two state [ref:no-ref]

---
+++ scripts/phones.component.js
@@ -0,0 +1,62 @@
+import {PhonesCatalogComponent} from "./phones-catalog/phones-catalog.component.js";
+import {BaseComponent} from "./shared/component/base/base.component.js";
+import {PhonesService} from "./phones.service.js";
+import {PhoneDetailsComponent} from "./phone-details/phone-details.component.js";
+
+console.log(PhonesService);
+
+export class PhonesPageComponent extends BaseComponent {
+
+    constructor({element}) {
+        super({element});
+        this._render();
+        this._catalog = new PhonesCatalogComponent({
+            element: this._element.querySelector('.phones-catalog'),
+            phones: PhonesService.getAll(),
+            onPhoneSelected: (phoneId) => {
+                this._catalog.hide();
+                this._phoneDetails.show(PhonesService.getOneById(phoneId));
+            }
+        })
+
+        this._phoneDetails = new PhoneDetailsComponent({
+            element: this._element.querySelector('.phone-details'),
+        })
+    }
+
+    _render() {
+        this._element.innerHTML = `  <div class="row">
+
+    <!--Sidebar-->
+    <div class="col-md-2">
+      <section>
+        <p>
+          Search:
+          <input>
+        </p>
+
+        <p>
+          Sort by:
+          <select>
+            <option value="name">Alphabetical</option>
+            <option value="age">Newest</option>
+          </select>
+        </p>
+      </section>
+
+      <section>
+        <p>Shopping Cart</p>
+        <ul>
+          <li>Phone 1</li>
+          <li>Phone 2</li>
+          <li>Phone 3</li>
+        </ul>
+      </section>
+    </div>
+
+    <!--Main content-->
+    <div class="col-md-10 phones-catalog"> </div>
+    <div class="col-md-10 phone-details"> </div>
+  </div>`
+    }
+}
